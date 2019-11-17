025592ab73eab5e89089be8871c1905683aec03f
Kurpotlar <46979029+Kurpotlar@users.noreply.github.com>
2019-11-17 13:33:14 -0500

    Add files via upload
    started the classLibrary

---
+++ SKADE/ClassLibrary/ClassLibrary/Logins.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Logins
+    {
+        public int ID { get; set; }
+
+        public int posID { get; set; }
+
+        public string userName { get; set; }
+
+        public string passWord { get; set; }
+
+        
+
+    }
+}
